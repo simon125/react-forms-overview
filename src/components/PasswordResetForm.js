ba08dd40e6f5eebcb5cd9c2b2560f3251f043d23
Szymon Oleszek <s.oleszek@polsource.com>
2019-04-01 17:04:53 +0200

    start working on react forms: main goals 1) use formik 2) use styled components 3) clean code/good-practise

---
+++ src/components/PasswordResetForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class PasswordResetForm extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                }}>
+
+
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default PasswordResetForm;
