9ec6062ec32e1b886182bcfada3999ab77d4ce38
Szymon Oleszek <szymqwerty@gmail.com>
2019-04-04 00:14:18 +0200

    added formik to register form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,34 +1,70 @@
 import React, { useState } from 'react';
+import { Formik } from 'formik';
 import FormContainer from '../hoc/FormContainer';
 import { SubmitButton, FormInput, Label, Title, LinkButton, Checkbox } from '../helpers/theme';
 
 
 const RegisterForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
     return (
         <React.Fragment>
             <Title>
                 Register Form
              </Title>
-            <Label>
-                Your Name
-             </Label>
-            <FormInput placeholder="Name" />
-            <Label>
-                Your Email address
-             </Label>
-            <FormInput placeholder="Email address" />
-            <Label>
-                Create Your password
-             </Label>
-            <FormInput placeholder="password" />
+            <Formik
+                initialValues={formState}
+                onSubmit={(values) => {
+                    const newFormState = { ...values }
+                    setFormState(newFormState);
+                }}
+                render={
+                    ({ values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting, }) => (
+                            <form onSubmit={handleSubmit}>
+                                <Label>Your Name</Label>
+                                <FormInput
+                                    autoComplete="off"
+                                    value={values.name}
+                                    onChange={handleChange}
+                                    name="name"
+                                    placeholder="Name" />
+                                <Label>Your Email address</Label>
+                                <FormInput
+                                    autoComplete="off"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    name="email"
+                                    placeholder="Email address" />
+                                <Label>Create Your password</Label>
+                                <FormInput
+                                    autoComplete="off"
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    name="password"
+                                    type="password"
+                                    placeholder="password" />
 
-            <SubmitButton type="submit">Sign up</SubmitButton>
+                                <SubmitButton type="submit">Sign up</SubmitButton>
+                            </form>
+                        )
+                } />
             <Checkbox type="checkbox" name="termsOfService" id="termsOfService" />
             I agree to the&nbsp;
-            <LinkButton type="button">Terms of Service</LinkButton>
+            <LinkButton
+                type="button">Terms of Service</LinkButton>
         </React.Fragment>
     )
 }
-
 
 export default FormContainer(RegisterForm);
